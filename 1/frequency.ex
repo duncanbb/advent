47cda7b0ddcaa305ce6819be581bd39516d3323a
Duncan Beebe <duncan.beebe@gmail.com>
2018-12-01 23:26:16 -0500

    day 1

--- 1/frequency.ex
+++ 1/frequency.ex
@@ -1,8 +1,26 @@
 defmodule FindFrequency do
-  def fromFile(filePath) do 
+  def frequenciesFromFile(filePath) do 
     { :ok, contents } = File.read(filePath)
     frequenciesAsStrings = contents |> String.split("\n", trim: true);
-    frequencies = frequenciesAsStrings |> Enum.map(&String.to_integer/1)
-    Enum.sum(frequencies)
+    frequenciesAsStrings |> Enum.map(&String.to_integer/1)
   end
+
+  def findFinalFrequency(filePath) do
+    Enum.sum(frequenciesFromFile(filePath))
+  end
+
+  def findDoubleFrequency(filePath) do
+    frequenciesFromFile(filePath)
+    |> Stream.cycle()
+    |> Enum.reduce_while({0, MapSet.new([0])}, fn i, { current, seen } ->
+      frequency = current + i
+
+      if MapSet.member?(seen, frequency) do
+        {:halt, frequency}
+      else 
+        {:cont, {frequency, MapSet.put(seen, frequency)}}
+      end
+    end)
+  end
+
 end
